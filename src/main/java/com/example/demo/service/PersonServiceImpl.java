1dd699270951fca4eaf9b7530fdaf63bd537057a
Jaison Zachariah <qburst.jaison@daimler.com>
2020-12-19 00:16:05 +0530

    Code clean up

--- src/main/java/com/example/demo/service/PersonServiceImpl.java
+++ src/main/java/com/example/demo/service/PersonServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.demo.service;
 
 import com.example.demo.repository.PersonRepository;
 import com.example.demo.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,34 +10,35 @@ import java.util.List;
 @Service("personService")
 public class PersonServiceImpl implements PersonService{
 
-    private final PersonRepository personDao;
+    private final PersonRepository personRepository;
 
+    @Autowired
     public PersonServiceImpl(PersonRepository personDao) {
-        this.personDao = personDao;
+        this.personRepository = personDao;
     }
 
     @Override
     public Person addPerson(Person person){
-        return personDao.save(person);
+        return personRepository.save(person);
     }
 
     @Override
     public List<Person> getAllPersons(){
-        return personDao.findAll();
+        return personRepository.findAll();
     }
 
     @Override
     public Person getPersonById(Long id) {
-        return personDao.findById(id).orElse(null);
+        return personRepository.findById(id).orElse(null);
     }
 
     @Override
     public void deletePersonById(Long id) {
-        personDao.deleteById(id);
+        personRepository.deleteById(id);
     }
 
     @Override
     public Person updatePersonById(Long id, Person person) {
-        return personDao.save(person);
+        return personRepository.save(person);
     }
 }
