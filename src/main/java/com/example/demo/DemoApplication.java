07dea6d406d7a162abdb7ac3e90c18042cb727a6
Jaison Zachariah <qburst.jaison@daimler.com>
2020-12-19 15:23:51 +0530

    # Added property files for DEV and PROD # Accessing data from environment variable

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,31 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class  DemoApplication {
+public class  DemoApplication implements CommandLineRunner {
+
+	private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+
+	@Autowired
+	private Environment env;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		String message = env.getProperty("messagefromUser");
+		logger.info(message);
+	}
 }
